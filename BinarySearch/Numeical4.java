a964412679bfedc9c5927c2184f00ae20e724ce1
GowdaHemannth <hemanthagalahalli@gmail.com>
2025-02-01 15:31:25 +0530

    Added Numerical4.java

---
+++ BinarySearch/Numeical4.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+public class Numeical4 {
+    static int Sorted(int arr[],int Start,int End,int Ele){
+        while(Start<=End){
+            int mid=(Start+End)/2;
+
+            if(Ele==arr[mid]){
+                return mid;
+            }
+            else if(Ele==arr[mid-1]&&mid-1<=Start){
+                return mid-1;
+            }
+            else if(Ele==arr[mid+1]&&mid+1<=End){
+                return mid+1;
+            }
+            
+            if(Ele>arr[mid]){
+                Start=mid+2;
+            }
+            else{
+                End=mid-2;
+            }
+
+        }
+        return 0;
+    }
+    public static void main(String[] args){
+        int arr[]={10,30,20,40,50};
+        int Start=0;
+        int n=arr.length-1;
+        int End=n;
+        int Ele=20;
+        System.out.println(Sorted(arr, Start, End, Ele));
+    }
+    
+}
