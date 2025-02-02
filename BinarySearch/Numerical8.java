541246cdf46d9a88d6adbfa4d1e04614ff65916c
GowdaHemannth <hemanthagalahalli@gmail.com>
2025-02-02 16:31:44 +0530

    Added Numerical8.java

---
+++ BinarySearch/Numerical8.java
@@ -0,0 +1,50 @@
+package BinarySearch;
+
+public class Numerical8 {
+    static int Peek(int arr[],int Start,int End){
+        while(Start<=End){
+            int mid=(Start+End)/2;
+            if(mid>0&&mid<=End-1){
+                if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
+                    return arr[mid];
+                }
+                else if(arr[mid]<arr[mid+1]){
+                    Start=mid+1;
+                }
+                else{
+                    End=mid-1;
+                }
+
+            }
+            else if(mid==0){
+                if(arr[0]>arr[1]){
+                    return arr[0];
+                }
+                else{
+                    return arr[1];
+                }
+
+            }
+            else if(mid==End){
+                if(arr[End]>arr[End-1]){
+                    return arr[End];
+                }
+                else{
+                    return arr[End-1];
+                }
+            }
+
+        }
+        return 0;
+    }
+    public static void main(String [] args){
+        int arr[]={5,10,8,15,25,30};
+        int Start=0;
+        int End=arr.length-1;
+      System.out.println(Peek(arr, Start, End))  ;
+        
+
+
+    }
+    
+}
