f43007986ba6fe6c61debaca3d2e1688679a85d6
GowdaHemannth <hemanthagalahalli@gmail.com>
2025-02-02 10:38:52 +0530

    Added Numerica6.java

---
+++ BinarySearch/Numerical6.java
@@ -0,0 +1,34 @@
+package BinarySearch;
+
+public class Numerical6 {
+    public static char FindCielChar(String str,int Start,int End,char s){
+       char res = '\0';
+        while(Start<=End){
+            int mid=(Start+End)/2;
+            if(str.charAt(mid)==s){
+                Start=mid+1;
+
+            }
+            else if(str.charAt(mid)>s){
+                res=str.charAt(mid);
+                End=mid-1;
+            }
+            else{
+                
+                Start=mid+1;
+            }
+
+        }
+        return res;
+
+    }
+    public static void main(String []args){
+        String str="abcdfg";
+        int Start=0;
+        char s='f';
+        int End=str.length()-1;
+        System.out.println(FindCielChar(str, Start, End, s));
+
+    }
+    
+}
