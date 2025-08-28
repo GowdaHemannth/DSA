9d198149504f51be9fe474dac81ec851d8896cdf
GowdaHemannth <hemanthagalahalli@gmail.com>
2025-08-28 09:33:01 +0530

    the file added

--- Recursion/Duplicates.java
+++ Recursion/Duplicates.java
@@ -1,6 +1,9 @@
 package Recursion;
 
 public class Duplicates{
+
+    // Here the Main goal is to Remove the Duplicates from the String Like we are Having the String Like 'aabanb'
+    // So the final output will be 'abn
     public static void Function(int idx,StringBuilder sb,String str,boolean map[]){
         if(idx==str.length()){
          System.out.println(sb);
@@ -12,10 +15,14 @@ public class Duplicates{
         }
         else  {
            map[ch-'a']=true;
-           Function(idx +1, sb.append(str), str, map);
+           Function(idx +1, sb.append(ch), str, map);
         }
     }
     public static void main(String[] args) {
+     StringBuilder sb=new StringBuilder();
+     String str="aabnb";
+   boolean map[] = new boolean[26];
+  Function(0, sb, str, map);
         
     }
 }
---
+++ Stack/ExtraSpace.jav
@@ -0,0 +1,4 @@
+// Here we are implemanting Stack with extra Space
+// Here we are implemensting the Function where if we remove certain elemsnt then Call that Function then that Function Should Give us the Value 
+
+// Hence thats the Case we are imlemantig the Min Function
